f18dfbe6f86d28dde8f28a40c62f2cfcd3223e9c
jayanth <83051900+jayan@users.noreply.github.com>
2024-05-09 19:19:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker build -t nginx .
 
+# Incremented count for image name
+IMAGE_COUNT=$(($(docker images | grep -c "^react") + 1))
+
+# Build and tag the Docker image
+docker build -t react${IMAGE_COUNT} .
