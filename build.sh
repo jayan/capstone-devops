84f859303635f0aad6991097330c0207bca5306b
jayanth <83051900+jayan@users.noreply.github.com>
2024-05-10 11:31:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 IMAGE_COUNT=$(($(docker images | grep -c "^react") + 1))
 
 # Build and tag the Docker image without using cache
-docker build --no-cache -t react${IMAGE_COUNT} .
+docker build --no-cache -t "react:${IMAGE_COUNT}" .
 
 # Echo the image name
-echo "Built Docker image: react${IMAGE_COUNT}"
+echo "Built Docker image: react:${IMAGE_COUNT}"
